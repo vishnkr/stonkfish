b72971aee5ccd44d2960e4be87b7be3ab714e5ca
Vishwa Shankar <vishwas1020@gmail.com>
2021-10-24 00:53:04 -0400

    replace primitive-types with numext-fixed-uint for bitboard

--- src/utils/bitboard.rs
+++ src/utils/bitboard.rs
@@ -1,17 +1,19 @@
-use primitive_types::U256;
+use numext_fixed_uint::U256;
+
+
 pub type Bitboard = U256;
 
 pub fn to_pos(x:u8,y:u8) -> usize{
     ((x*16)+y).into()
 }
 
+
 pub fn to_string(bitboard:&Bitboard)->String{
-    println!("reached");
     let mut bb_string = String::new().to_owned();
     for i in 0..16{
         for j in 0..16{
             let index = to_pos(i,j);
-            if bitboard.bit(index){
+            if bitboard.bit(index).unwrap_or(false){
                 bb_string.push_str("1 ");
             } else{
                 bb_string.push_str("- " );
@@ -19,7 +21,6 @@ pub fn to_string(bitboard:&Bitboard)->String{
         }
         bb_string.push_str("\n");
     }
-    println!("{}",bitboard);
     println!("{}",bb_string);
     bb_string
 }
