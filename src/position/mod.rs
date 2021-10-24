8545396a64b1cf651a1b138ba99c73ab69e026e3
Vishwa Shankar <vishwas1020@gmail.com>
2021-10-24 00:54:31 -0400

    bitboard mutable ref fix, evaluator initial work

--- src/position/mod.rs
+++ src/position/mod.rs
@@ -1,5 +1,5 @@
 
-use crate::utils::bitboard::{Bitboard,to_pos};
+use crate::utils::bitboard::{Bitboard,to_pos,to_string};
 
 
 pub enum PieceType{
@@ -13,10 +13,10 @@ pub enum PieceType{
 }
 
 pub struct Piece{
-    piece_type:PieceType,
-    bitboard: Bitboard,
+    pub piece_type:PieceType,
+    pub bitboard: Bitboard,
     piece_repr: char,
-    player:u8
+    pub player:u8
 }
 
 impl Piece{
@@ -52,14 +52,15 @@ impl Piece{
     }
 }
 
+
 pub struct PieceSet{
-    player:u8,
-    king:Piece,
-    queen:Piece,
-    rook:Piece,
-    bishop:Piece,
-    knight:Piece,
-    pawn:Piece,
+    pub player:u8,
+    pub king:Piece,
+    pub queen:Piece,
+    pub rook:Piece,
+    pub bishop:Piece,
+    pub knight:Piece,
+    pub pawn:Piece,
     //custom:Vec<Piece>
 }
 
@@ -78,9 +79,9 @@ impl PieceSet{
 }
 
 pub struct Position{
-    turn: u8,
+    pub turn: u8,
     pub dimensions:Dimensions,
-    pieces: Vec<PieceSet>
+    pub pieces: Vec<PieceSet>
 }
 
 #[derive(Debug,PartialEq)]
@@ -110,17 +111,21 @@ pub fn get_dimensions(fen_first_part:Vec<String>)-> Dimensions{
 }
 
 impl Position{
+
+    pub fn new()->Position{
+        Position::load_from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string())
+    }
     pub fn load_from_fen(fen:String) -> Position{
         println!("Erg {}",fen.split_whitespace().nth(0).as_deref().unwrap_or("nop"));
         let board_data:String = fen.split(" ").collect(); //_whitespace().nth(0).as_deref().unwrap_or("nop");
         let dimensions:Dimensions = get_dimensions(board_data.split("/").map(|s| s.to_string()).collect());
-        let white_piece_set:PieceSet = PieceSet::new(super::WHITE);
-        let black_piece_set:PieceSet = PieceSet::new(super::BLACK);
+        let mut white_piece_set:PieceSet = PieceSet::new(super::WHITE);
+        let mut black_piece_set:PieceSet = PieceSet::new(super::BLACK);
         let mut turn:u8 = 0;
         let mut fen_part = 0;
         let mut sec_digit = 0;
-        let mut col =0;
-        let mut row =0;
+        let mut col:u8 = 0;
+        let mut row = 0;
         let mut count:u32;
         let mut _castle_white_kingside = false;
         let mut _castle_white_queenside = false;
@@ -141,10 +146,25 @@ impl Position{
                         count = c.to_digit(RADIX).unwrap_or(0);
                         if i+1<dimensions.width.into() && (fen.as_bytes()[i+1] as char).is_digit(RADIX){
                             sec_digit = c.to_digit(RADIX).unwrap_or(0);
-                        } else{
-                            col+=sec_digit*10+count;
+                        } else {
+                            col+=(sec_digit*10+count) as u8;
                             sec_digit=0;
                         }
+                    } else {
+                        
+                        let bitboard: &mut Bitboard = match c.to_ascii_lowercase(){
+                            'p'=> if c.is_ascii_lowercase(){&mut white_piece_set.pawn.bitboard} else {&mut black_piece_set.pawn.bitboard}
+                            'k'=> if c.is_ascii_lowercase(){&mut white_piece_set.king.bitboard} else {&mut black_piece_set.king.bitboard}
+                            'b'=> if c.is_ascii_lowercase(){&mut white_piece_set.bishop.bitboard} else {&mut black_piece_set.bishop.bitboard}
+                            'n'=> if c.is_ascii_lowercase(){&mut white_piece_set.knight.bitboard} else {&mut black_piece_set.knight.bitboard}
+                            'r'=> if c.is_ascii_lowercase(){&mut white_piece_set.rook.bitboard} else {&mut black_piece_set.rook.bitboard}
+                            'q'=> if c.is_ascii_lowercase(){&mut white_piece_set.queen.bitboard} else {&mut black_piece_set.queen.bitboard}
+                            _=> continue
+                        };
+                        bitboard.set_bit(to_pos(row,col),true);
+                        println!("piece- {}",c);
+                        to_string(&bitboard);
+                        col+=1
                     }
                 }
                 1=>{
@@ -162,20 +182,11 @@ impl Position{
                 }
                 _ => continue,
             }
-            let bb_index = to_pos(row, col as u8);
-            let mut bitboard: Bitboard;
-            match c.to_ascii_lowercase(){
-                'p'=> if c.is_ascii_lowercase(){bitboard = white_piece_set.pawn.bitboard} else {bitboard = black_piece_set.pawn.bitboard}
-                'k'=> if c.is_ascii_lowercase(){bitboard = white_piece_set.king.bitboard} else {bitboard = black_piece_set.king.bitboard}
-                'b'=> if c.is_ascii_lowercase(){bitboard = white_piece_set.bishop.bitboard} else {bitboard = black_piece_set.bishop.bitboard}
-                'n'=> if c.is_ascii_lowercase(){bitboard = white_piece_set.knight.bitboard} else {bitboard = black_piece_set.knight.bitboard}
-                'r'=> if c.is_ascii_lowercase(){bitboard = white_piece_set.rook.bitboard} else {bitboard = black_piece_set.rook.bitboard}
-                'q'=> if c.is_ascii_lowercase(){bitboard = white_piece_set.queen.bitboard} else {bitboard = black_piece_set.queen.bitboard}
-                _=>{bitboard = Bitboard::zero()}
-            }
-            bitboard = bitboard | (Bitboard::one() << to_pos(row, col as u8));
         }
-        Position{dimensions:dimensions,turn:turn,pieces:Vec::new()}
+        let mut pieces = Vec::new();
+        pieces.push(white_piece_set);
+        pieces.push(black_piece_set);
+        Position{dimensions:dimensions,turn:turn,pieces:pieces}
     }
 }
 
@@ -185,8 +196,8 @@ mod position_tests{
     use super::*;
     #[test]
     fn test_fen_to_position_conversion(){
-        let fen:String = "8/5p1k/8/p2p1P2/5q2/P1PbN2p/7P/2Q3K1 w - - 1 44".to_string();
-        let dimensions:Dimensions = Dimensions{width:8,height:8};
+        let fen:String = "12/5p1k4/12/p2p1P6/5q6/P1PbN2p4/7P4/2Q3K5/12/12/12/12 w - - 1 44".to_string();
+        let dimensions:Dimensions = Dimensions{width:12,height:12};
         let turn:u8=0;
         let result: Position = Position::load_from_fen(fen);
         assert_eq!(result.dimensions,dimensions);
